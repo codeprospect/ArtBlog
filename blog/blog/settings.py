2dc99bf9a32d66ac4027fccbc8d6945914d6f9d2
SteveSpec <wambuastephen001@gmail.com>
2022-02-08 13:03:09 +0300

    Rich Text Editor

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blogapp.apps.BlogappConfig',
     'crispy_forms',
+    'ckeditor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
